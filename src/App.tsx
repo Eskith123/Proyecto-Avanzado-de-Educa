9d709c8775e966973aa3c9a23f7213700c503eb6
Eskith123 <yoskinjulio08@gmail.com>
2025-12-06 11:43:32 -0400

    eliminacion de cartas

--- src/App.tsx
+++ src/App.tsx
@@ -17,56 +17,6 @@ function App() {
       vida={75}
       velocidad={85}
     />
-
-   <CardDetail
-      ataque={608}
-      nombre="Armarouge"
-      defensa={100}
-      descripcion="El conjunto de armadura de Armarouge pertenecía a un guerrero distinguido y es la fuente de la energía utilizada para sus movimientos de tipo Psíquico. También utiliza las capacidades psíquicas de esta armadura para controlar la energía de fuego dentro de su cuerpo y desatar ataques."
-      imagen="https://img.pokemondb.net/artwork/avif/armarouge.avif"
-      numero={936}
-      tipo="FUEGO Y PSIQUICO"
-      vida={85}
-      velocidad={75}
-    />
-
-    <CardDetail
-      ataque={90}
-      nombre="Hatterene"
-      defensa={95}
-      descripcion="Si haces demasiado ruido a su alrededor, corres el riesgo de que las garras de su tentáculo te destrozen. Este Pokémon también es conocido como la Bruja del Bosque."
-      imagen="https://img.pokemondb.net/artwork/avif/hatterene.avif"
-      numero={858}
-      tipo="PSIQUICO Y HADA"
-      vida={57}
-      velocidad={29}
-       />
-
-     <CardDetail
-      ataque={98}
-      nombre="Toxtricity"
-      defensa={95}
-      descripcion="Tiene un órgano eléctrico en el pecho. Mientras genera electricidad, llena su entorno con lo que suena como el rasgueo de un bajo."
-      imagen="https://img.pokemondb.net/artwork/large/toxtricity-amped.jpg"
-      numero={849}
-      tipo="ELECTRICO Y VENENO"
-      vida={75}
-      velocidad={75}
-       />
-
-       <CardDetail
-      ataque={125}
-      nombre="Golisopod"
-      defensa={140}
-      descripcion="Hace lo que sea por conseguir la victoria. Si el rival se descuida, aprovecha para asestarle un golpe letal con sus pequeñas garras frontales."
-      imagen="https://img.pokemondb.net/artwork/large/golisopod.jpg"
-      numero={768}
-      tipo="BICHO Y AGUA"
-      vida={75}
-      velocidad={40}
-       /> 
-
-      
 </div>
 );
 }
